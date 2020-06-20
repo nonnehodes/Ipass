738544385ea3615bcc64e218b8447c5348299682
nonnehodes1 <nonne.hodes@student.hu.nl>
2020-06-20 13:44:52 +0200

    eerste sql statements

--- project_files/services/db_data.py
+++ project_files/services/db_data.py
@@ -3,15 +3,14 @@ import sqlite3
 def setup_connection():
     return sqlite3.connect("../../database/nefub.sqlite")
 
-def get_team():
+def get_club():
     con = setup_connection()
     cur = con.cursor()
     cur.execute("""SELECT DISTINCT thuisclub FROM dames_competitie""")
     rows = cur.fetchall()
 
-    team_lijst = []
+    club_lijst = []
     for row in rows:
-        team_lijst += row
-    print(team_lijst)
+        club_lijst += row
+    print(club_lijst)
 
-get_team()
