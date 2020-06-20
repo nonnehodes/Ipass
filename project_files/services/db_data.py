8932f131a4c9af3e56912e8ee51baafad9ab11e0
nonnehodes1 <nonne.hodes@student.hu.nl>
2020-06-20 14:28:07 +0200

    get team scores;

--- project_files/services/db_data.py
+++ project_files/services/db_data.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 def setup_connection():
     return sqlite3.connect("../../database/nefub.sqlite")
@@ -14,3 +15,33 @@ def get_club():
         club_lijst += row
     print(club_lijst)
 
+def get_team_score(team1, team2):
+    con = setup_connection()
+    cur = con.cursor()
+    cur.execute("""SELECT datum, thuisteam, thuisscore FROM dames_competitie WHERE thuisteam=? and uitteam=?""", (team1, team2))
+    thuis_score = cur.fetchall()
+    output = []
+    for item in thuis_score:
+        scores = {}
+        scores['datum'] = item[0]
+        scores['team'] = item[1]
+        scores['uit_thuis'] = 'thuis'
+        scores['score'] = item[2]
+        output.append(scores)
+
+    cur.execute("""SELECT datum, uitteam, uitscore FROM dames_competitie WHERE thuisteam=? and uitteam=?""", (team2, team1))
+    uit_score = cur.fetchall()
+    for item in uit_score:
+        scores = {}
+        scores['datum'] = item[0]
+        scores['team'] = item[1]
+        scores['uit_thuis'] = 'uit'
+        scores['score'] = item[2]
+        output.append(scores)
+
+    df = pd.DataFrame(output)
+    df['datum'] = pd.to_datetime(df.datum)
+    df = df.sort_values(by='datum').reset_index()
+    return df
+
+get_team_score("UFC Utrecht 1", "Sonics")
